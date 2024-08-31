6532d01bd84106ee9feb6f1abee2747c2ca314ba
theruler624 <mloucksmloucks@gmail.com>
2024-08-31 14:10:29 -0400

    addedGame

---
+++ 3d/scripts/player.gd
@@ -0,0 +1,7 @@
+extends RigidBody3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("left", "right")
+	input.z = Input.get_axis("forward", "back")
